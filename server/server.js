f22a45276e45ddde8204eb90afa2f952f389e837
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-04-18 08:57:45 +0100

    Added route DELETE /snippets/:id

--- server/server.js
+++ server/server.js
@@ -38,6 +38,25 @@ app.get('/snippets', (request, response) => {
         .catch(e => response.status(400).send());
 });
 
+// DELETE /snippets/:id
+app.delete('/snippets/:id', (request, response) => {
+    let id = request.params.id;
+    // If id is an invalid id return 404
+    if (!ObjectID.isValid(id)) {
+        return response.status(404).send();
+    }
+    // Get the snippet
+    Snippet.findByIdAndDelete(id)
+        .then(snippet => {
+            // if no snippet was deleted shoot error
+            if (!snippet) {
+                return response.status(404).send();
+            }
+            response.status(200).send({snippet});
+        })
+        .catch(e => response.status(400).send());
+})
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
